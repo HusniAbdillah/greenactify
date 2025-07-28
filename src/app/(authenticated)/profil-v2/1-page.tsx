666d656bc97a504d1a5541751c1bc62cf5429ed9
Gilang Agung <prakoso89.09@gmail.com>
2025-07-28 21:57:11 +0700

    Profile page testing

---
+++ src/app/(authenticated)/profil-v2/1-page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function page(){
+    return(
+        <div className='min-h-screen bg-[#BBE8C3]'>
+            <h2>
+                Profil
+            </h2>
+            <div className='flex flex-row justify-start gap-5 items-center'>
+                
+            </div>
+        </div>
+    )
+}
+
